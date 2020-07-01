d358e3b3e2f76f84b1f8eec26dc3e00516fd4007
KEYS248 <kleinjdavid@gmail.com>
2020-07-01 12:39:01 -0700

    Update submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -26,7 +26,7 @@ Demultiplexer: \n\t\
         sh ${PIPELINE_DIR}/submit_job.sh --demultiplex --fastq_dir /path/to/fastq/ --run_dir /path/to/runfolder/ \n\n\
 VAF filter: \n\t\
     Required: --input_vcf <arg> and --output_tsv <arg> \n\t\
-    Optional: --input_blacklist <arg>, --output_blacklist <arg>, --combine_blacklists, --vaf (default 0.1), --dp (default 5), --mq (default 30) \n\t\
+    Optional: --input_blacklist <arg>, --output_blacklist <arg>, --combine_blacklists, --vaf <arg> (default 0.1), --dp <arg> (default 5), --mq <arg> (default 30) \n\t\
     Run like: \n\t\t\
         sh ${PIPELINE_DIR}/submit_job.sh --vaf_filter --input_vcf /path/to/unfiltered_variants.vcf --output_tsv /path/to/filtered_variants.tsv \n\n\
 Monovar variant caller: \n\t\
