1d0cb2ce2b9f4d1975da18a9755918025317c50b
David Klein <kleinjdavid@gmail.com>
2021-07-09 15:22:53 -0700

    open comments

--- scripts/circle_map.sh
+++ scripts/circle_map.sh
@@ -20,29 +20,29 @@ export PATH=/home/groups/cgawad/python_libs/bin:$PATH
 ml py-numpy/1.19.2_py36 py-biopython/1.70_py27 py-pandas/1.0.3_py36
 ml python/3.6.1
 
-# echo "### Sort BAM by read name ### - START: $(date)"
-# samtools sort -n -o ${SAMPLE}_qname${BAM_SUFFIX} ${BAM_DIR}/${SAMPLE}${BAM_SUFFIX}
-# echo "### Sort BAM by read name ### - END: $(date)"
-# 
-# echo "### Sort BAM by leftmost mapping coordinates ### - START: $(date)"
-# samtools sort -o ${SAMPLE}_sorted${BAM_SUFFIX} ${SAMPLE}${BAM_SUFFIX}
-# samtools index ${SAMPLE}_sorted${BAM_SUFFIX}
-# echo "### Sort BAM by leftmost mapping coordinates ### - END: $(date)"
-# 
-# echo "### Extract circular DNA rearrangements ### - START: $(date)"
-# Circle-Map ReadExtractor -i ${SAMPLE}_qname${BAM_SUFFIX} -o ${SAMPLE}_circular_read_candidates${BAM_SUFFIX}
-# echo "### Extract circular DNA rearrangements ### - END: $(date)"
-# 
-# echo "### Sort read candidates by coordinates ### - START: $(date)"
-# samtools sort -o ${SAMPLE}_sorted_circular_read_candidates${BAM_SUFFIX} ${SAMPLE}_circular_read_candidates${BAM_SUFFIX}
-# samtools index ${SAMPLE}_sorted_circular_read_candidates${BAM_SUFFIX}
-# echo "### Sort read candidates by coordinates ### - END: $(date)"
-# 
-# echo "### Detect circular DNA ### - START: $(date)"
-# Circle-Map Realign -i ${SAMPLE}_sorted_circular_read_candidates${BAM_SUFFIX} \
-#     -qbam ${SAMPLE}_qname${BAM_SUFFIX} -sbam ${SAMPLE}_sorted${BAM_SUFFIX} \
-#     -fasta $REF_FASTA -o ${SAMPLE}_unknown_circle.bed
-# echo "### Detect circular DNA ### - END: $(date)"
+echo "### Sort BAM by read name ### - START: $(date)"
+samtools sort -n -o ${SAMPLE}_qname${BAM_SUFFIX} ${BAM_DIR}/${SAMPLE}${BAM_SUFFIX}
+echo "### Sort BAM by read name ### - END: $(date)"
+
+echo "### Sort BAM by leftmost mapping coordinates ### - START: $(date)"
+samtools sort -o ${SAMPLE}_sorted${BAM_SUFFIX} ${SAMPLE}${BAM_SUFFIX}
+samtools index ${SAMPLE}_sorted${BAM_SUFFIX}
+echo "### Sort BAM by leftmost mapping coordinates ### - END: $(date)"
+
+echo "### Extract circular DNA rearrangements ### - START: $(date)"
+Circle-Map ReadExtractor -i ${SAMPLE}_qname${BAM_SUFFIX} -o ${SAMPLE}_circular_read_candidates${BAM_SUFFIX}
+echo "### Extract circular DNA rearrangements ### - END: $(date)"
+
+echo "### Sort read candidates by coordinates ### - START: $(date)"
+samtools sort -o ${SAMPLE}_sorted_circular_read_candidates${BAM_SUFFIX} ${SAMPLE}_circular_read_candidates${BAM_SUFFIX}
+samtools index ${SAMPLE}_sorted_circular_read_candidates${BAM_SUFFIX}
+echo "### Sort read candidates by coordinates ### - END: $(date)"
+
+echo "### Detect circular DNA ### - START: $(date)"
+Circle-Map Realign -i ${SAMPLE}_sorted_circular_read_candidates${BAM_SUFFIX} \
+    -qbam ${SAMPLE}_qname${BAM_SUFFIX} -sbam ${SAMPLE}_sorted${BAM_SUFFIX} \
+    -fasta $REF_FASTA -o ${SAMPLE}_unknown_circle.bed
+echo "### Detect circular DNA ### - END: $(date)"
 
 echo "### Parsing annotated variants that fall within circular regions ### - START: $(date)"
 if [ "$FINAL_SNPS" != "0" ]; then
