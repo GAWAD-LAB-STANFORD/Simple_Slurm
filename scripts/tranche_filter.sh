51da6da7b2c7cbaf53a6a2718b1a10ffaa0369b8
David Klein <kleinjdavid@gmail.com>
2021-08-07 11:03:06 -0700

    sigprofiler patch

--- scripts/tranche_filter.sh
+++ scripts/tranche_filter.sh
@@ -206,7 +206,8 @@ cut -f 1,2,4,5 ${PROJECT}.tranche_${TRANCHE}.merged.snp_vqsr.snp_only.${ANNOVAR_
     | uniq > ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_reformat.tsv
 Rscript ${SCRIPT_DIR}/SigProfiler_1_Bedtools_Reformat.R \
     ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_reformat.tsv \
-    ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_before_input.tsv ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_after_input.tsv
+    ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_before_input.tsv \
+    ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_after_input.tsv
 bedtools getfasta -fi $REF_FASTA \
     -bed ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_before_input.tsv \
     -bedOut > ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_before_output.tsv
@@ -217,16 +218,15 @@ Rscript ${SCRIPT_DIR}/SigProfiler_2_Trinucleotide_Reformat.R \
     ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_reformat.tsv \
     ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_before_output.tsv \
     ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_after_output.tsv \
-    ${PROJECT}.tranche_${TRANCHE}.trinucleotide.tsv \
-    ${PROJECT}.tranche_${TRANCHE}.temp_sigprofiler_input.tsv \
+    ${PROJECT}.tranche_${TRANCHE}.temp_trinucleotide.tsv \
+    ${PROJECT}.tranche_${TRANCHE}.sigprofiler_input.tsv \
     ${SCRIPT_DIR}/Mutation_Types.tsv
-python3 -u ${SCRIPT_DIR}/SigProfiler_3_Extractor.py \
-    ${PROJECT}.tranche_${TRANCHE}.temp_sigprofiler_input.tsv \
-    ${PROJECT}.tranche_${TRANCHE}_SigProfiler_Results $RESULTS_DIR
-rm ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_reformat.tsv 
-rm ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_before_input.tsv ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_after_input.tsv 
-rm ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_before_output.tsv ${PROJECT}.tranche_${TRANCHE}.temp_bedtools_after_output.tsv
-rm ${PROJECT}.tranche_${TRANCHE}.temp_sigprofiler_input.tsv
+echo "You'll need to run SigProfiler manually on your own computer until the SigProfiler developers can fix the multiprocessing issue"
+echo "Here's a command you can use manually:"
+echo -e "\tpython3 -u SigProfiler_3_Extractor.py ${PROJECT}.tranche_${TRANCHE}.sigprofiler_input.tsv ${PROJECT}.tranche_${TRANCHE}_SigProfiler_Results /full/path/to/results/dir/"
+# python3 -u ${SCRIPT_DIR}/SigProfiler_3_Extractor.py \
+#     ${PROJECT}.tranche_${TRANCHE}.sigprofiler_input.tsv \
+#     ${PROJECT}.tranche_${TRANCHE}_SigProfiler_Results $RESULTS_DIR
 echo "### Computing mutational signature with SigProfiler ### - END: $(date)"
 
 echo -e "END: $(date)\nRuntime: $(($(date +%s)-$START_TIME)) seconds"
