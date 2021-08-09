8d82981d66caa6d0f96e897a6cd117485933d4fd
David Klein <kleinjdavid@gmail.com>
2021-08-08 20:03:23 -0700

    fix sig profiler

--- scripts/SigProfiler_3_Extractor.py
+++ scripts/SigProfiler_3_Extractor.py
@@ -6,4 +6,5 @@ results_dir = sys.argv[3]
 
 from SigProfilerExtractor import sigpro as sig
 
-sig.sigProfilerExtractor("matrix", output_dir, "{}/{}".format(results_dir, input_file), reference_genome="GRCh38", minimum_signatures=3, maximum_signatures=10, nmf_replicates=20)
+if __name__ == '__main__':
+    sig.sigProfilerExtractor("matrix", output_dir, "{}/{}".format(results_dir, input_file), reference_genome="GRCh38", minimum_signatures=3, maximum_signatures=10, nmf_replicates=20)
