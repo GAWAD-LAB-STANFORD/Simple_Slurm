eb11b21690e77dbc2f79ecdcf8328682231522e5
David Klein <kleinjdavid@gmail.com>
2022-01-07 19:18:53 -0800

    more info printed

--- scripts/ginkgo_cnv.sh
+++ scripts/ginkgo_cnv.sh
@@ -42,7 +42,7 @@ while [ "$1" != "" ]; do
     shift
 done
 
-echo -e "START: $(date)\nBam dir: ${BAM_DIR}\nResults dir: $RESULTS_DIR\nBam regex: ${BAM_REGEX}\nBam suffix: ${BAM_SUFFIX}\nKb bin size: $KB_BIN_SIZE"
+echo -e "START: $(date)\nBam dir: ${BAM_DIR}\nResults dir: $RESULTS_DIR\nBam regex: ${BAM_REGEX}\nBam suffix: ${BAM_SUFFIX}\nKb bin size: $KB_BIN_SIZE\nFull work dir: $FULL_WORK_DIR"
 if [ -z $RESULTS_DIR ]; then
     RESULTS_DIR=$BAM_DIR
 fi
